bafb648f5fd1bd5ea068b799036ea01c540487bc
Youness <younessfad@gmail.com>
2023-10-25 16:49:26 +0100

    changz title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>iPhone Register</title>
+    <title>iPhone 3</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
